109f44110ae76c31c587451560f3982a844b7f61
caionardesfiscal <39067201+caionardesfiscal@users.noreply.github.com>
2018-05-09 13:31:33 -0300

    8099 - Usando recurso que foi alterado em outra history (#18)

---
+++ TesteGIT/src/com/features/feature8099/Feature8099.java
@@ -0,0 +1,12 @@
+package com.features.feature8099;
+
+import com.util.ClasseGenerica;
+
+public class Feature8099 {
+
+	public static void main(String[] args) {
+		System.out.println("nova feature 8099");
+		
+		ClasseGenerica.metodoApenasFeature8094();
+	}
+}
