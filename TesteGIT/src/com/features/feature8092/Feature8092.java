6e4f0944d5f3a81f02a0931bc78d1429c98e266f
caionardesfiscal <39067201+caionardesfiscal@users.noreply.github.com>
2018-05-09 11:57:44 -0300

    reintegração (#10)
    * 8093 com metodo util (#5)
    
    * Nova feature 8092 com Metodo util (#6)
    
    * Nova feature 8092 com Metodo util
    
    * ajuste
    
    * Sprint02/feature 8092 (#7)
    
    * Nova feature 8092 com Metodo util
    
    * ajuste
    
    * ajuste fino
    
    * reintegração (#8) (#9)
    
    * 8093 com metodo util (#5)
    
    * Nova feature 8092 com Metodo util (#6)
    
    * Nova feature 8092 com Metodo util
    
    * ajuste
    
    * Sprint02/feature 8092 (#7)
    
    * Nova feature 8092 com Metodo util
    
    * ajuste
    
    * ajuste fino

---
+++ TesteGIT/src/com/features/feature8092/Feature8092.java
@@ -0,0 +1,11 @@
+package com.features.feature8092;
+
+import com.util.ClasseGenerica;
+
+public class Feature8092 {
+
+	public static void main(String[] args) {
+		ClasseGenerica.metodoGenerico();
+		ClasseGenerica.novoMetodoFeature8092();
+	}
+}
---
+++ TesteGIT/src/com/features/feature8093/Feature8093.java
@@ -0,0 +1,11 @@
+package com.features.feature8093;
+
+import com.util.ClasseGenerica;
+
+public class Feature8093 {
+
+	public static void main(String[] args) {
+		ClasseGenerica.metodoGenerico();
+		ClasseGenerica.metodoFeature8093();
+	}
+}
