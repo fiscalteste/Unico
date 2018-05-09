748a3ad0ef32ab8612f62e8155dcd7cd8af0c94e
caionardesfiscal <caio.nardes@fiscaltech.com.br>
2018-05-09 11:32:53 -0300

    8093 com metodo util

--- TesteGIT/src/com/util/ClasseGenerica.java
+++ TesteGIT/src/com/util/ClasseGenerica.java
@@ -5,4 +5,8 @@ public class ClasseGenerica {
 	public static void metodoGenerico() {
 		System.out.println("Criado na 8090");
 	}
+
+	public static void metodoFeature8093() {
+		System.out.println("Feaytre 8093");
+	}
 }
