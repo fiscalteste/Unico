6d5dafb3a7994693e385c93051abbe0c351e9509
caionardesfiscal <39067201+caionardesfiscal@users.noreply.github.com>
2018-05-09 13:31:11 -0300

    8098 - metodo que nao deve dar merge (#17)

---
+++ TesteGIT/src/com/features/feature8098/Feature8098.java
@@ -0,0 +1,12 @@
+package com.features.feature8098;
+
+import com.util.ClasseGenerica;
+
+public class Feature8098 {
+
+	public static void main(String[] args) {
+		System.out.println("Minha feature!");
+		
+		ClasseGenerica.novoMEtodo8098();
+	}
+}
