fd86f0db4103b7fe7d6e835be3dd170d93e513d1
caionardesfiscal <caio.nardes@fiscaltech.com.br>
2018-05-09 11:53:23 -0300

    ajuste fino

--- TesteGIT/src/com/util/ClasseGenerica.java
+++ TesteGIT/src/com/util/ClasseGenerica.java
@@ -5,13 +5,13 @@ public class ClasseGenerica {
 	public static void metodoGenerico() {
 		System.out.println("Criado na 8090");
 	}
-  
-  // <<<<<<< sprint02/feature_8092
+
+	// <<<<<<< sprint02/feature_8092
 	public static void novoMetodoFeature8092() {
 		System.out.println("Sou unico mesmo.");
 		System.out.println("Meu metodo 8092 que estava dando conflito");
 		System.out.println("Sou unico mesmo.");
-  }
+	}
 
 	public static void metodoFeature8093() {
 		System.out.println("Feaytre 8093");
