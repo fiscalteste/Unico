919a96300299410c2d2df2426603eef3e3d17dd7
caionardesfiscal <caio.nardes@fiscaltech.com.br>
2018-05-09 17:33:22 -0300

    8099 - quero ver isso no Kraken

--- TesteGIT/src/com/features/feature8099/Feature8099.java
+++ TesteGIT/src/com/features/feature8099/Feature8099.java
@@ -7,6 +7,10 @@ public class Feature8099 {
 	public static void main(String[] args) {
 		System.out.println("nova feature 8099");
 		
+		for (int i = 0; i < 213; i++) {
+			System.out.println("Repete feature 8099");
+		}
+		
 		ClasseGenerica.metodoApenasFeature8094();
 	}
 }
