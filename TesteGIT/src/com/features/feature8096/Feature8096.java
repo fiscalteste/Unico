c482db1ac0984c7d1665826731bb6b95ae24125c
caionardesfiscal <39067201+caionardesfiscal@users.noreply.github.com>
2018-05-09 12:59:38 -0300

    Sprint03 feature8096 (#13)
    * Revert "Sprint02/feature 8092 (#7)"
    
    This reverts commit a977db10d7b3342a64ccdedcad08309bf5dd3503.
    
    * Feature 8094 - Criou novo util e utiliza 8093
    
    * 8096 - Nao possui merge, mais utiliza algo

---
+++ TesteGIT/src/com/features/feature8096/Feature8096.java
@@ -0,0 +1,11 @@
+package com.features.feature8096;
+
+import com.util.ClasseGenerica;
+
+public class Feature8096 {
+
+	public static void main(String[] args) {
+		System.out.println("eu utilizo alguma coisa, mais nao trago merges!");
+		ClasseGenerica.metodoApenasFeature8094();
+	}
+}
