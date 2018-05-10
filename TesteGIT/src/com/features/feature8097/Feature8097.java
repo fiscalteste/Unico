4684ec764fc2fbe30a497278b9987d91d603bf81
caionardesfiscal <39067201+caionardesfiscal@users.noreply.github.com>
2018-05-10 12:59:24 -0300

    Finalização Sprint04 (#26)
    * 8097 - alterou 8094 (#16)
    
    * 8099 - Usando recurso que foi alterado em outra history (#18)
    
    * Sprint04 feature8099 (#19)
    
    * 8099 - Usando recurso que foi alterado em outra history
    
    * 8099 - quero ver isso no Kraken
    
    * 8097 - alterou 8094 (#20)
    
    * 8098 - metodo que nao deve dar merge (#21)
    
    * Sprint04 feature8099 (#24)
    
    * 8099 - Usando recurso que foi alterado em outra history
    
    * 8099 - quero ver isso no Kraken

---
+++ TesteGIT/src/com/features/feature8097/Feature8097.java
@@ -0,0 +1,10 @@
+package com.features.feature8097;
+
+import com.util.ClasseGenerica;
+
+public class Feature8097 {
+
+	public static void main(String[] args) {
+		ClasseGenerica.metodoApenasFeature8094("Fala isso aqui!");
+	}
+}
---
+++ TesteGIT/src/com/features/feature8099/Feature8099.java
@@ -0,0 +1,16 @@
+package com.features.feature8099;
+
+import com.util.ClasseGenerica;
+
+public class Feature8099 {
+
+	public static void main(String[] args) {
+		System.out.println("nova feature 8099");
+    
+		for (int i = 0; i < 213; i++) {
+			System.out.println("Repete feature 8099");
+		}
+
+		ClasseGenerica.metodoApenasFeature8094();
+	}
+}
