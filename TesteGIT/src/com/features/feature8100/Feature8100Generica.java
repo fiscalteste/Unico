381e84cf0ac25986304c21003f376e3ec3368fef
caionardesfiscal <caio.nardes@fiscaltech.com.br>
2018-05-10 13:32:04 -0300

    8100 cria o objeto

---
+++ TesteGIT/src/com/features/feature8100/Feature8100Generica.java
@@ -0,0 +1,14 @@
+package com.features.feature8100;
+
+public class Feature8100Generica {
+
+	public static void main(String[] args) {
+		System.out.println("Minha operação geral!");
+	}
+	
+	public static void calcularConta(Integer conta, String nome) {
+		for (int i = 0; i < conta; i++) {
+			System.out.println("Conta n" + conta + " em nome do " + nome);
+		}
+	}
+}
