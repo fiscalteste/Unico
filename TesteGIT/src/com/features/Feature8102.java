38126c95ee93b86036baf1eb02914ad3ba537879
caionardesfiscal <39067201+caionardesfiscal@users.noreply.github.com>
2018-05-10 15:58:15 -0300

    Feature 8102 (#31)
    * 8100 cria o objeto
    
    * 8102 - alteração no mesmo metodo

---
+++ TesteGIT/src/com/features/Feature8102.java
@@ -0,0 +1,10 @@
+package com.features;
+
+import com.features.feature8100.Feature8100Generica;
+
+public class Feature8102 {
+
+	public static void main(String[] args) {
+		Feature8100Generica.calcularConta(231, "");
+	}
+}
